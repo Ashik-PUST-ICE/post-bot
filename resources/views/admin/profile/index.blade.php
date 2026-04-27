0ea2a2bf3ddfe98aed37799292e80a20583a003f
ashik-zainik-lab <ashik.200607.zan@gmail.com>
2026-04-27 16:49:51 +0600

    feat: create admin profile and password management blade views

--- resources/views/admin/profile/index.blade.php
+++ resources/views/admin/profile/index.blade.php
@@ -5,12 +5,7 @@
 @section('content')
     <div data-aos="fade-up" data-aos-duration="1000" class="p-sm-30 p-15">
         <div class="row rg-20">
-            <div class="col-xl-3">
-                <div class="bg-white p-sm-25 p-15 bd-one bd-c-stroke bd-ra-8">
-                    @include('admin.setting.sidebar')
-                </div>
-            </div>
-            <div class="col-xl-9">
+            <div class="col-xl-12">
                 <div class="bd-one bd-c-stroke bd-ra-8 bd-bl-ra-0 bd-br-ra-0 pt-25 px-sm-25 px-10 bg-white">
                     <ul class="d-flex flex-wrap rg-20 zList-one">
                         <li>
